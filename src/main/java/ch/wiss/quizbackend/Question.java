a71ea67f5f9d4f47a0be93f5e1cc5bf782a99da8
Silvio apa <apa.silvionh@gmail.com>
2026-06-13 09:34:12 +0200

    01A: Erster Question-Endpoint mit Spring Boot

---
+++ src/main/java/ch/wiss/quizbackend/Question.java
@@ -0,0 +1,49 @@
+package ch.wiss.quizbackend;
+import java.util.List;
+
+public class Question {
+
+    private String id;
+    private String text;
+    private String category;
+    private String difficulty;
+    private List<String> answers;
+    private String correctAnswer;
+
+
+    public Question(String id, String text, String category, String difficulty, List<String> answers, String correctAnswer) {
+        this.id = id;
+        this.text = text;
+        this.category = category;
+        this.difficulty = difficulty;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+}
+
---
+++ src/test/java/ch/wiss/quizbackend/QuizbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wiss.quizbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
